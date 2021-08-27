b3a9c234ae32977c7c5e926b8345f20b1c69100c
ZQR <473578042@qq.com>
2021-08-27 19:42:24 +0800

    create project

--- src/main/java/com/examle/gitdemo/GitDemo.java
+++ src/main/java/com/examle/gitdemo/GitDemo.java
@@ -3,5 +3,6 @@ package com.examle.gitdemo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("111");
+        System.out.println("1312321312");
     }
 }
